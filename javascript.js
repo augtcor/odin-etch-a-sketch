9b0aa9012c64b326f56f0ab80d9030b3640b5388
augcor <augusto.t.correa@gmail.com>
2022-10-12 17:51:33 -0300

    Update newGrid function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 let container = document.getElementById('grid');
 
+let gridSize = document.getElementById('grid-size');
+gridSize.addEventListener('click', changeGridSize);
+
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.classList.add('squares');
@@ -16,4 +19,22 @@ function changeBackground() {
         let divId = this.id;
         let color = document.getElementById(divId);
         color.style.backgroundColor = 'black';
+}
+
+function changeGridSize() {
+    let gridInput = prompt('Choose your grid size:');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (let i = 0; i < gridInput * gridInput; i++) {
+        let div = document.createElement('div');
+        div.classList.add('squares');
+        div.id = i;
+        container.appendChild(div);
+       
+        let newGrid = container.querySelectorAll('.squares');
+        newGrid.forEach(div => {
+            div.style.flexBasis = String(100 / gridInput) + '%';
+        });
+    }    
 }
