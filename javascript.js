a911da65ee800a5fadf4bb17be91e71be3b0132a
augcor <augusto.t.correa@gmail.com>
2022-10-12 18:01:34 -0300

    Format code for better legibility

--- javascript.js
+++ javascript.js
@@ -23,18 +23,29 @@ function changeBackground() {
 
 function changeGridSize() {
     let gridInput = prompt('Choose your grid size:');
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
+    
+    if (gridInput > 64 || gridInput < 2) {
+        alert('Not a valid number.')
+    } else {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        
+        for (let i = 0; i < gridInput * gridInput; i++) {
+            let div = document.createElement('div');
+            div.classList.add('squares');
+            div.id = i;
+            container.appendChild(div);
+        
+            let newGrid = container.querySelectorAll('.squares');
+            newGrid.forEach(div => {
+                div.style.flexBasis = String(100 / gridInput) + '%';
+            });
+        }
+        
+        squares = document.querySelectorAll('.squares');
+        squares.forEach(div => {
+            div.addEventListener('mouseenter', changeBackground);
+        });    
     }
-    for (let i = 0; i < gridInput * gridInput; i++) {
-        let div = document.createElement('div');
-        div.classList.add('squares');
-        div.id = i;
-        container.appendChild(div);
-       
-        let newGrid = container.querySelectorAll('.squares');
-        newGrid.forEach(div => {
-            div.style.flexBasis = String(100 / gridInput) + '%';
-        });
-    }    
 }
