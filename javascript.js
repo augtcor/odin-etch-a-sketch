7c2f383d459c5fae2866f77b54e52ef088541c74
augcor <augusto.t.correa@gmail.com>
2022-10-12 15:47:38 -0300

    Fix changeBackground function

--- javascript.js
+++ javascript.js
@@ -2,5 +2,18 @@ let container = document.getElementById('container');
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
+    div.classList.add('squares');
+    div.id = i;
     container.appendChild(div);
+}
+
+let squares = document.querySelectorAll('.squares');
+squares.forEach(div => {
+    div.addEventListener('mouseenter', changeBackground);
+});
+
+function changeBackground() {
+        let divId = this.id;
+        let color = document.getElementById(divId);
+        color.style.backgroundColor = 'black';
 }
